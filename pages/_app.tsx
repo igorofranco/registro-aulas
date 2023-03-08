35cc6c75edbb12f1686a98a6ee52c06c231d60e7
Igor Franco <igorofranco@gmail.com>
2023-03-07 21:42:22 -0300

    bugfix attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,14 +10,14 @@ import { classFormatsStore } from '../store/classFormatsStore';
 import Head from 'next/head';
 // import { userSlice } from '../features/user/userSlice';
 // import { useRouter } from 'next/router';
-import User from '../types/user';
+// import User from '../types/user';
 // import StudentApi from '../Api/StudentApi';
 // import { studentsSlice } from '../features/student/studentsSlice';
 
 function MyApp ({ Component, pageProps }: AppProps) {
   const themeColor = '1976d2';
-  const [user, setUser] = React.useState<User>(userStore.getState());
-  const [isLoading, setLoading] = React.useState<boolean>(true);
+  // const [user, setUser] = React.useState<User>(userStore.getState());
+  const [isLoading] = React.useState<boolean>(true);
   // userStore.subscribe(() => {
   //   setUser(userStore.getState());
   // });
