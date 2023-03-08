70116421cb9f9599cdf81e7f01641f01a0f13475
Igor Franco <igorofranco@gmail.com>
2023-03-07 21:47:05 -0300

    bugfix attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,38 +8,38 @@ import { instrumentsStore } from '../store/instrumentsStore';
 import studentsStore from '../store/studentsStore';
 import { classFormatsStore } from '../store/classFormatsStore';
 import Head from 'next/head';
-// import { userSlice } from '../features/user/userSlice';
-// import { useRouter } from 'next/router';
-// import User from '../types/user';
-// import StudentApi from '../Api/StudentApi';
-// import { studentsSlice } from '../features/student/studentsSlice';
+import { userSlice } from '../features/user/userSlice';
+import { useRouter } from 'next/router';
+import User from '../types/user';
+import StudentApi from '../Api/StudentApi';
+import { studentsSlice } from '../features/student/studentsSlice';
 
 function MyApp ({ Component, pageProps }: AppProps) {
   const themeColor = '1976d2';
-  // const [user, setUser] = React.useState<User>(userStore.getState());
-  const [isLoading] = React.useState<boolean>(true);
-  // userStore.subscribe(() => {
-  //   setUser(userStore.getState());
-  // });
-  // React.useEffect(() => {
-  //   userStore.dispatch(userSlice.actions.autoLoginByLocalStorage());
-  // }, []);
-  //
-  // React.useEffect(() => {
-  //   setLoading(true);
-  //   if (!userStore.getState()?.id) {
-  //     if (typeof window !== 'undefined') {
-  //       const router = useRouter();
-  //       router.push('/login').then();
-  //     }
-  //     return;
-  //   }
-  //   StudentApi.getAll()
-  //     .then(res => {
-  //       studentsStore.dispatch(studentsSlice.actions.setStudents(res));
-  //     })
-  //     .then(() => setLoading(false));
-  // }, []);
+  const [user, setUser] = React.useState<User>(userStore.getState());
+  const [isLoading, setLoading] = React.useState<boolean>(true);
+  userStore.subscribe(() => {
+    setUser(userStore.getState());
+  });
+  React.useEffect(() => {
+    userStore.dispatch(userSlice.actions.autoLoginByLocalStorage());
+  }, []);
+
+  React.useEffect(() => {
+    setLoading(true);
+    if (!userStore.getState()?.id) {
+      if (typeof window !== 'undefined') {
+        const router = useRouter();
+        router.push('/login').then();
+      }
+      return;
+    }
+    StudentApi.getAll()
+      .then(res => {
+        studentsStore.dispatch(studentsSlice.actions.setStudents(res));
+      })
+      .then(() => setLoading(false));
+  }, [user]);
 
   if (isLoading) {
     return <div>carregando</div>;
