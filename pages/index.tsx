c83f89acb8af14389f9d91abc3ca7ccabca979db
Igor Franco <igorofranco@gmail.com>
2023-03-09 21:42:28 -0300

    date nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import StudentApi from '../Api/StudentApi';
 import User from '../types/user';
 import userStore from '../store/userStore';
-import { IconButton, Menu, MenuItem, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Button, IconButton, Menu, MenuItem, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import MainSpeedDial from '../components/MainSpeedDial';
 import studentsStore from '../store/studentsStore';
 import { studentsSlice } from '../features/student/studentsSlice';
@@ -31,6 +31,13 @@ const daysInEnglish = [
   'SATURADAY'
 ];
 
+const monthsInPt = [
+  'Jan', 'Fev', 'Mar',
+  'Abr', 'Mai', 'Jun',
+  'Jul', 'Ago', 'Set',
+  'Out', 'Nov', 'Dez'
+];
+
 function studentToRow (student: Student): StudentRow {
   return {
     id: student.id || 0,
@@ -38,17 +45,18 @@ function studentToRow (student: Student): StudentRow {
     instrument: student.instrument?.instrument,
     modality: student.classFormat.modality,
     duration: student.classFormat.timeMinutes,
-    price: student.classFormat.price / 100,
+    price: student.classFormat.price,
     student
   };
 }
 
 const Home: NextPage = () => {
   const [user, setUser] = React.useState<User>(userStore.getState());
-  const [total, setTotal] = React.useState<number>(0);
   const [students, setStudents] = React.useState<StudentRow[]>([]);
   const [isLoading, setLoading] = React.useState<boolean>(false);
   const [menuAnchorEl, setMenuAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedMonth, setSelectedMonth] = React.useState<number>(new Date().getMonth() + 1);
+  const [selectedYear, setSelectedYear] = React.useState<number>(new Date().getFullYear());
   userStore.subscribe(() => {
     setUser(userStore.getState());
   });
@@ -61,13 +69,41 @@ const Home: NextPage = () => {
     fetchStudents();
   }, []);
 
-  React.useEffect(() => {
-    let newTotal = 0;
-    for (const student of students) {
-      newTotal += student.price;
-    }
-    setTotal(newTotal);
-  }, [students]);
+  function previousMonth (): void {
+    setSelectedMonth(prevState => {
+      if (prevState === 1) {
+        previousYear();
+        return 12;
+      }
+      return prevState - 1;
+    });
+  }
+  function nextMonth (): void {
+    setSelectedMonth(prevState => {
+      if (prevState === 12) {
+        nextYear();
+        return 1;
+      }
+      return prevState + 1;
+    });
+  }
+  function previousYear (): void {
+    setSelectedYear(prevState => prevState - 1);
+  }
+  function nextYear (): void {
+    setSelectedYear(prevState => prevState + 1);
+  }
+  function resetDate (): void {
+    const date = new Date();
+    setSelectedMonth(date.getMonth() + 1);
+    setSelectedYear(date.getFullYear());
+  }
+
+  function rs (value: number): string {
+    let v = value.toString();
+    v = v.slice(0, v.length - 2) + ',' + v.slice(v.length - 2);
+    return `R$ ${v}`;
+  }
 
   function fetchStudents (): void {
     setLoading(true);
@@ -79,17 +115,16 @@ const Home: NextPage = () => {
   }
 
   function montlyValueByStudent (student: Student): number {
-    const date = new Date();
-    const selectedMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    const numberOfDaysInSelecetdMonth = selectedMonth.getDate();
+    const month = new Date(selectedYear, selectedMonth, 0);
+    const numberOfDaysInSelecetdMonth = month.getDate();
     let numberOfClasses = 0;
-    for (let i = 0; i <= numberOfDaysInSelecetdMonth; i++) {
-      selectedMonth.setDate(i);
-      if (selectedMonth.getDay() === daysInEnglish.indexOf(student.daysOfWeek.toString())) {
+    for (let i = 1; i <= numberOfDaysInSelecetdMonth; i++) {
+      month.setDate(i);
+      if (month.getDay() === daysInEnglish.indexOf(student.daysOfWeek.toString())) {
         numberOfClasses++;
       }
     }
-    return (numberOfClasses * student.classFormat.price) / 100;
+    return (numberOfClasses * student.classFormat.price);
   }
 
   function getMonthTotal (): number {
@@ -105,68 +140,96 @@ const Home: NextPage = () => {
   }
 
   return (
-    <main className='mt-2 flex flex-col items-center justify-center'>
-      <Table className='max-w-full overflow-x-auto' sx={{ width: '720px' }}>
-        <TableHead>
-          <TableRow>
-            <TableCell><span className='text-2xl' title='Nome'>🙋</span></TableCell>
-            <TableCell><span className='text-2xl' title='Valor da Aula'>💰</span>/mês</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {students.map(s => {
-            const id = `aluno-${s.id}-${s.name}-${s.instrument}`;
-            return (
-              <TableRow
-                key={id}
-              >
-                <TableCell>{s.name}</TableCell>
-                <TableCell>R$ {montlyValueByStudent(s.student)},00</TableCell>
-                <TableCell>
-                  <IconButton
-                    size='small'
-                    onClick={e => setMenuAnchorEl(e.currentTarget)}
-                  >
-                    <FontAwesomeIcon icon={faEllipsisVertical} />
-                  </IconButton>
-                  <Menu
-                    open={!!menuAnchorEl}
-                    anchorEl={menuAnchorEl}
-                    onClose={() => setMenuAnchorEl(null)}
-                  >
-                    <MenuItem
-                      className='flex gap-2'
-                      onClick={() => {
-                        StudentApi.delete(s.id)
-                          .then(() => fetchStudents());
-                        setMenuAnchorEl(null);
-                      }}
+    <main className="mt-2 flex flex-col items-center justify-center">
+      <header className="font-bold text-center">
+        <div className="text-3xl">{rs(getMonthTotal())}</div>
+        <div className="text-xl">em</div>
+        <div className="flex items-center gap-2">
+          <Button
+            size='small'
+            variant='outlined'
+            onClick={previousMonth}
+          >
+            {'<-'}
+          </Button>
+          <span
+            className="text-2xl hover:cursor-pointer"
+            onClick={resetDate}
+          >
+            {monthsInPt[selectedMonth - 1]}/{selectedYear}
+          </span>
+          <Button
+            size='small'
+            variant='outlined'
+            onClick={nextMonth}
+          >
+            {'->'}
+          </Button>
+        </div>
+      </header>
+      <main>
+        <Table
+          className="max-w-full overflow-x-auto"
+          sx={{ width: '720px' }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell><span
+                className="text-2xl"
+                title="Nome"
+              >🙋</span></TableCell>
+              <TableCell><span
+                className="text-2xl"
+                title="Valor da Aula"
+              >💰</span>/mês</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map(s => {
+              const id = `aluno-${s.id}-${s.name}-${s.instrument}`;
+              return (
+                <TableRow
+                  key={id}
+                >
+                  <TableCell>{s.name}</TableCell>
+                  <TableCell>{rs(montlyValueByStudent(s.student))}</TableCell>
+                  <TableCell>
+                    <IconButton
+                      size="small"
+                      disabled={isLoading || !user.token}
+                      onClick={e => setMenuAnchorEl(e.currentTarget)}
                     >
-                      <FontAwesomeIcon
-                        icon={faTrashAlt}
-                        className="text-red-800"
-                      />
-                      <span>Deletar</span>
-                    </MenuItem>
-                  </Menu>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-          <TableRow key='aluno-total'>
-            <TableCell>Total/semana</TableCell>
-            <TableCell>R$ {total},00</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Total/mês</TableCell>
-            <TableCell>R$ {getMonthTotal()},00</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      {!isLoading || !user.token ? <MainSpeedDial /> : null}
+                      <FontAwesomeIcon icon={faEllipsisVertical} />
+                    </IconButton>
+                    <Menu
+                      open={!!menuAnchorEl}
+                      anchorEl={menuAnchorEl}
+                      onClose={() => setMenuAnchorEl(null)}
+                    >
+                      <MenuItem
+                        className="flex gap-2"
+                        onClick={() => {
+                          StudentApi.delete(s.id)
+                            .then(() => fetchStudents());
+                          setMenuAnchorEl(null);
+                        }}
+                      >
+                        <FontAwesomeIcon
+                          icon={faTrashAlt}
+                          className="text-red-800"
+                        />
+                        <span>Deletar</span>
+                      </MenuItem>
+                    </Menu>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </main>
+      {!isLoading && user.token ? <MainSpeedDial /> : null}
     </main>
   );
 };
